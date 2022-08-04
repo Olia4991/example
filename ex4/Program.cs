f922305009221d193354ca5ab6a421bada0c25f8
olia <olia4991@mail.ru>
2022-08-04 18:29:32 +0300

    ex44

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,5 +1,4 @@
-﻿int numberA = 31;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
 int result = (numberA + numberB);
 Console.WriteLine(result);
